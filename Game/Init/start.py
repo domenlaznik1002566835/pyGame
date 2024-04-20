e99f8a2a42794710133312927753427102ca43dc
domenlaznik1002566836 <domen.laznik@student.um.si>
2024-04-20 14:46:17 +0200

    Requirements commit

---
+++ Game/Init/start.py
@@ -0,0 +1,18 @@
+# Imports
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Constraints
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+BOX_WIDTH = 50
+BOX_HEIGHT = 50
+BACKGROUND_COLOR = (255, 255, 255)
+BOX_COLOR = (255, 0, 0)
+
+
+# Display window
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
