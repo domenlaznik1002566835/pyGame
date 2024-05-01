03294a824d6a35486f7f7ccd6bd3baa31da39205
domenlaznik1002566836 <domen.laznik@student.um.si>
2024-05-02 00:12:35 +0200

    Failed github commit and push

--- Game/Init/Resources/Objects/MeleWeapon.py
+++ Game/Init/Resources/Objects/MeleWeapon.py
@@ -1,4 +1,5 @@
 from .Weapon import Weapon
+import pygame as pg
 
 class MeleWeapon(Weapon):
     def __init__(self, name, damage, width, height, speed, duel_wield=False):
@@ -7,3 +8,21 @@ class MeleWeapon(Weapon):
 
     def set_swing_coordinates(self, coordinates):
         self.swing_coordinates = coordinates
+
+    def draw(self, window, x, y, height, width):
+        x_change = 0
+        y_change = 0
+        if self.stage == -1:
+            x_change = self.coordinates[0]
+            y_change = self.coordinates[1]
+        elif self.stage < len(self.swing_coordinates):
+            x_change = self.swing_coordinates[self.stage][0]
+            y_change = self.swing_coordinates[self.stage][1]
+        if self.rotation == 1:
+            self.x = x - x_change - self.width
+            self.y = y - y_change - self.height // 2
+            pg.draw.rect(window, (0, 0, 255), (x - x_change - self.width, y - y_change - self.height//2, self.width, self.height))
+        elif self.rotation == -1:
+            self.x = x + width + x_change
+            self.y = y - y_change - self.height // 2
+            pg.draw.rect(window, (0, 0, 255), (x + width + x_change, y - y_change - self.height//2, self.width, self.height))
--- Game/Init/Resources/Objects/Weapon.py
+++ Game/Init/Resources/Objects/Weapon.py
@@ -20,24 +20,6 @@ class Weapon:
     def set_use_coordinates(self, coordinates):
         self.swing_coordinates = coordinates
 
-    def draw(self, window, x, y, height, width):
-        x_change = 0
-        y_change = 0
-        if self.stage == -1:
-            x_change = self.coordinates[0]
-            y_change = self.coordinates[1]
-        elif self.stage < len(self.swing_coordinates):
-            x_change = self.swing_coordinates[self.stage][0]
-            y_change = self.swing_coordinates[self.stage][1]
-        if self.rotation == 1:
-            self.x = x - x_change - self.width
-            self.y = y - y_change - self.height // 2
-            pg.draw.rect(window, (0, 0, 255), (x - x_change - self.width, y - y_change - self.height//2, self.width, self.height))
-        elif self.rotation == -1:
-            self.x = x + width + x_change
-            self.y = y - y_change - self.height // 2
-            pg.draw.rect(window, (0, 0, 255), (x + width + x_change, y - y_change - self.height//2, self.width, self.height))
-
     def get_hitbox(self):
         return pg.Rect(self.x, self.y, self.width, self.height)
 
